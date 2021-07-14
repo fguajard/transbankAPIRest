1f543c7e05badab0a2d48c86195485fd4b4d2706
fguajard <franciscogm31@gmail.com>
2021-07-14 10:56:38 -0400

    template server con http en node

---
+++ server.js
@@ -0,0 +1,57 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+http
+  .createServer((req, res) => {
+    
+    if (req.url == "/") {
+      fs.readFile("public/index.html", "utf8", (err, html) => {
+        if (err) {
+          console.log(err);
+          res.statusCode = 500;
+          res.end(JSON.stringify(err));
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(html);
+      });
+    } 
+    
+    else if (req.url == "/estilos") {
+      fs.readFile("public/style.css", (err, css) => {
+        if (err) {
+          console.log(err);
+          res.statusCode = 500;
+          res.end(JSON.stringify(err));
+        }
+        res.writeHead(200, { "Content-Type": "text/css" });
+        res.end(css);
+      });
+    } 
+
+    else if (req.url == "/javascript") {
+        fs.readFile("public/script.js", (err, js) => {
+          if (err) {
+            console.log(err);
+            res.statusCode = 500;
+            res.end(JSON.stringify(err));
+          }
+          res.writeHead(200, { "Content-Type": "application/javascript" });
+          res.end(js);
+        });
+      }    
+
+    else{
+        fs.readFile("public/404.html", "utf8", (err, errorPage) => {
+            if (err) {
+              console.log(err);
+              res.statusCode = 500;
+              res.end(JSON.stringify(err));
+            }
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(errorPage);
+          });
+        
+    }
+  })
+  .listen(3000, () => console.log("Servidor encendido"));
